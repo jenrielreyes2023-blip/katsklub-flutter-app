664c694a4c705eefd67edc5d46b402c1048ed70c
Ubuntu <ubuntu@katsklub.top>
2026-07-29 23:42:23 +0000

    feat(notifications): trigger message_in sound when general notifications arrive while browsing outside thread (v1.0.12+25)

--- lib/services/feed_service.dart
+++ lib/services/feed_service.dart
@@ -618,6 +618,7 @@ class FeedService {
       if (notification['isRead'] != true) {
         unreadNotificationsNotifier.value =
             unreadNotificationsNotifier.value + 1;
+        MessageSoundService.playIncoming();
       }
     });
 
